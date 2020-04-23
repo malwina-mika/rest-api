9e81b74b1dcd396df116e405b85e7d7458f3ce1e
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-04-23 16:05:08 +0200

    Change content of endpoints

--- server.js
+++ server.js
@@ -1,29 +1,51 @@
 const express = require('express');
 const cors = require('cors');
+const db = require('./db.js');
+const uuidv4 = require('uuid/v4');
 
 const app = express();
-
-const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-];
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
 app.get('/testimonials', (req, res) => {
-  res.render('db');
+  res.json(db.testimonials);
 });
 
 app.get('/testimonials/:id', (req, res) => {
-  res.render(db.map(item => item.id));
+  res.json(db.testimonials.filter(item => item.id == req.params.id));
 });
 
 app.get('/testimonials/random', (req, res) => {
-  res.render(db);
+  res.json(db.testimonials[Math.floor(Math.random() * db.testimonials.length)]);
 });
 
+app.post('/testimonials', (req, res) => {
+  const { author, text } = req.body;
+  const id = uuidv4();
+
+  const newTestimonial = {
+    id: id,
+    author: author,
+    text: text
+  }
+
+  db.testimonials.push(newTestimonial);
+  res.json({ message: 'ok' });
+});
+
+app.put('/testimonials/:id', (req, res) => {
+  res.json({ message: 'ok' });
+});
+
+app.delete('/testimonials/:id', (req, res) => {
+  const item = db.testimonials.find(item => item.id == req.params.id);
+  const index = db.testimonials.indexOf(item)
+
+  db.testimonials.splice(index, 1)
+  res.json({ message: 'ok' });
+});
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
