d64c7c7499fbd1877cba810a7afd8f16c631ca15
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-04-24 00:29:10 +0200

    Change in server before publish on heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -16,12 +17,17 @@ app.use('/api', testimonials);
 app.use('/api', seats);
 app.use('/api', concerts);
 
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
 
